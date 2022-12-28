7bd431a3fa79af3d1bbf3594014ab5b30b9cd71d
Hasan Ablak <0hasanablak@gmail.com>
2022-12-28 16:09:16 +0300

    Interface'lerimizi bind ettim

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -6,6 +6,9 @@ use Illuminate\Support\ServiceProvider;
 use App\Interfaces\ITodosRepository;
 use App\Repository\TodosRepository;
 
+use App\Interfaces\IUsersRepository;
+use App\Repository\UsersRepository;
+
 class AppServiceProvider extends ServiceProvider
 {
 	/**
@@ -17,6 +20,10 @@ class AppServiceProvider extends ServiceProvider
 	{
 		$this->app->bind(ITodosRepository::class, function ($app) {
 			return new TodosRepository;
+		});
+
+		$this->app->bind(IUsersRepository::class, function ($app) {
+			return new UsersRepository;
 		});
 	}
 
