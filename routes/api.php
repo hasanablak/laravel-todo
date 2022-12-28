82f8ab354517727d359e64d9091816a69b3ab6e5
Hasan Ablak <0hasanablak@gmail.com>
2022-12-28 16:15:29 +0300

    api.php üzeriinde geliştirmeler
    api.php üzerinde sadece auth ile girilmesi gereken routeları düzenledim ve resource olarak tanımladığım routelarda fazladan routelar tanımlandığı için onları düzenlemek adına except ve only methodlarını uyguladım

--- routes/api.php
+++ routes/api.php
@@ -5,23 +5,27 @@ use App\Http\Controllers\UsersController;
 use App\Http\Controllers\ToDosController;
 use App\Http\Controllers\AuthController;
 use App\Http\Controllers\HomeController;
+use App\Http\Middleware\AdminMiddleware;
 
 
 Route::controller(AuthController::class)->group(function () {
 	Route::post('/login', 'login');
 	Route::post('/register', 'register');
-	Route::post('/logout', 'logout');
+	Route::post('/logout', 'logout')->middleware('auth:api');
 });
 
 
 
+
+
 Route::resource("users", UsersController::class)
-	->only(["index", "show", "store", "update"])
-	->middleware('auth:api');
+	->only(["index", "show", "update"])
+	->middleware(['auth:api', AdminMiddleware::class]);
+
 Route::resource("users.todos", TodosController::class)
-	->only(["index", "show", "store", "update"])
+	->except(["create", "edit"])
 	->middleware('auth:api');
 
 Route::post("users/{user}/todos/{todo}/restore", [TodosController::class, "restore"])->middleware('auth:api');
 
-Route::get('home-feed', [HomeController::class, "index"]);
+Route::get('home-feed', [HomeController::class, "index"])->middleware('auth:api');
