46ea89d9eda1ef35e3e2d808a6387381febed500
hasanablak <0hasanablak@gmail.com>
2023-01-03 13:04:21 +0300

    ExampleTest

--- tests/Feature/ExampleTest.php
+++ tests/Feature/ExampleTest.php
@@ -3,19 +3,65 @@
 namespace Tests\Feature;
 
 // use Illuminate\Foundation\Testing\RefreshDatabase;
+
+use App\Models\Todo;
 use Tests\TestCase;
+use App\Models\User;
+use Illuminate\Support\Facades\Log;
+use Illuminate\Support\Str;
 
 class ExampleTest extends TestCase
 {
-    /**
-     * A basic test example.
-     *
-     * @return void
-     */
-    public function test_the_application_returns_a_successful_response()
-    {
-        $response = $this->get('/');
+	/**
+	 * A basic test example.
+	 *
+	 * @return void
+	 */
 
-        $response->assertStatus(200);
-    }
+
+	public function test_kullanici_kendisine_todo_ekliyor()
+	{
+		$user = User::first();
+
+		$response = $this->actingAs($user)
+			->post('/api/users/' . $user->id . '/todos', [
+				"title"	=>	"test"
+			]);
+
+
+		$response->assertStatus(200);
+	}
+
+	public function test_admin_baskasina_todo_ekliyor()
+	{
+		$user = User::where('is_admin', 1)->first();
+
+		$anotherUser = User::inRandomOrder()->first();
+
+		$response = $this->actingAs($user)
+			->post('/api/users/' . $anotherUser->id . '/todos', [
+				"title"	=>	"test"
+			]);
+
+
+		$response->assertStatus(200);
+	}
+
+	public function test_admin_olmayan_kisi_baskasina_todo_eklemeye_calisirsa_kendisine_eklemis_olur()
+	{
+		$user = User::where('is_admin', 0)->first();
+
+		$anotherUser = User::inRandomOrder()->first();
+		$title = date("dmyhis");
+		$response = $this->actingAs($user)
+			->post('/api/users/' . $anotherUser->id . '/todos', [
+				"title"	=>	$title
+			]);
+
+		$todo = Todo::where('usersQid', $user->id)->latest()->first();
+
+		$this->assertEquals($title, $todo->title);
+
+		//$todo->assertJson(["title" => $randomStr]);
+	}
 }
